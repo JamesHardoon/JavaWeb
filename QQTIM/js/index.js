f3978a7119589bd63833f9f8557130ec5dd18716
JamesHardoon <1219308337@qq.com>
2018-11-26 23:10:30 +0800

    add

---
+++ QQTIM/js/index.js
@@ -0,0 +1,12 @@
+$(function () {
+	/*1.引入*/
+	/**
+	 * 2.结构 data-stellar-background-ratio="0.3"
+	 * 样式 bg 需要添加background-attachment: fixed;
+	 */
+	/*3.初始化插件*/
+	$.stellar({
+		horizontalScrolling: false,
+		responsive: true
+	});
+});
---
+++ 博雅互动 bootstrap版/js/relation.js
@@ -0,0 +1,16 @@
+window.onload =function(){
+	function $(id){
+		return document.getElementById(id);
+	}
+	
+	$('content').innerHTML = '<h1>敬请期待...</h1><br/>';
+	
+	$('content2').innerHTML = '<h4>FZ</h4>';
+							 
+	
+}
+
+// $(document).ready(function(){
+// 	$("#content").innerHTML='<h1>敬请期待...</h1><br/>';
+// 	$('content2').innerHTML = '<h4>Liu QZ</h4>';
+// });
